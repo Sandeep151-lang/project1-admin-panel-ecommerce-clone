6d4aa46facbbd6dc47dda74c7b52f7490c54619d
Sandeep <sandeepnandanwar92@gmail.com>
2022-03-03 23:53:21 +0530

    Update AdminDashboard.js

--- client/src/component/AdminDashboard.js
+++ client/src/component/AdminDashboard.js
@@ -58,7 +58,7 @@ const AdminDashboard = () => {
     const edit = async (_id) => {
         try {
             await axios.get(`/product/${_id}`)
-            history.push(`/product/${_id}`)
+            history.push(`/Product/${_id}`)
         } catch {
             console.log('error')
         }
